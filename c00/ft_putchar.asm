d1e427bd65db41e4bd2ec90ff613d8a2f4a0680c
6L45 <silas.dbr170@gmail.com>
2023-02-19 19:09:17 +0100

    ft_putchar

---
+++ c00/ft_putchar.asm
@@ -0,0 +1,22 @@
+section .text
+global ft_putchar
+
+ft_putchar:
+	push	rbp
+	mov		rbp, rsp
+	
+	sub		rsp, 8		; allocate 8 bytes for the buffer
+	
+	mov		byte [rsp], dil	; store the character in the buffer
+	
+	mov		rax, 1		; sys_write system call number
+	mov		rdi, 1		; stdout file descriptor
+	lea		rsi, [rsp]	; pointer to the buffer
+	mov		rdx, 1		; number of bytes to write
+	
+	syscall
+	
+	mov		rsp, rbp
+	pop		rbp
+	ret
+
---
+++ c00/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+extern void ft_putchar(char c);
+
+int main()
+{
+    ft_putchar('H');
+    ft_putchar('i');
+    ft_putchar('!');
+    ft_putchar(10);
+
+    return 0;
+}
+
